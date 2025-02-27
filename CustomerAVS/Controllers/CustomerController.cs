e652de0af4eb7f97782aac1a001fe3fe631e8f44
Ashl3yV <127549843+Ashl3yV@users.noreply.github.com>
2025-02-27 00:18:00 -0600

    Agregar archivos de proyecto.

---
+++ CustomerAVS/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using CustomerAVS.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CustomerAVS.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CustomerAVS.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private static string conexion = ConfigurationManager.ConnectionStrings["DBCustomer"].ToString();
+
+        public static List<customer> olista = new List<customer>();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlConnection cmd = new 
+            }
+        }
+    }
+}
---
+++ CustomerAVS/Models/customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerAVS.Models
+{
+	public class customer
+	{
+        public int ID { get; set; }
+        public int TotalVisits { get; set; }
+        public string AccountNumber { get; set; }
+        public string Company { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public DateTime AccountOpened { get; set; }
+        public DateTime LastVisit { get; set; }
+
+        
+
+    }
+}
